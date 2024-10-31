8f6da6f0944bfc4ec873799824a4f2cd5c78615d
Afonso Matumona <afonso.matumona@minfin.gov.ao>
2024-10-31 13:30:15 +0100

    feat: added modifiers in map(...).to(...)

--- src/main/java/io/github/afonsomatelias/Mapper/ListProcessor.java
+++ src/main/java/io/github/afonsomatelias/Mapper/ListProcessor.java
@@ -5,7 +5,9 @@ import java.util.List;
 import io.github.afonsomatelias.Callback.ICallbacks.CallbackV1;
 import io.github.afonsomatelias.Configurations.ConverterShared;
 import io.github.afonsomatelias.Mapper.Interfaces.IListProcessor;
+import io.github.afonsomatelias.Options.MappingActions;
 import io.github.afonsomatelias.Options.MappingListActions;
+import io.github.afonsomatelias.Options.Interfaces.IMappingActions;
 import io.github.afonsomatelias.Options.Interfaces.IMappingListActions;
 
 @SuppressWarnings("unchecked")
@@ -48,6 +50,28 @@ public class ListProcessor<S> extends Processor<S> implements IListProcessor<S> 
 			}
 
 			return (List<D>) this.toDestination(clazz);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
+	 * Creates a new instance of object provided, just like copy and paste with
+	 * different memory address
+	 * 
+	 * @return new object instance
+	 */
+	@Override
+	public <D extends S> S to(CallbackV1<IMappingActions<S, D>> modifier) {
+		try {
+			if (modifier != null) {
+				// Assing to object to be able to trick the compiler
+				Object modifierAsObject = actionOptions;
+
+				modifier.call((MappingActions<S, D>) modifierAsObject);
+			}
+
+			return (S) this.toDestination(source.getClass());
 		} catch (Exception e) {
 			return null;
 		}
