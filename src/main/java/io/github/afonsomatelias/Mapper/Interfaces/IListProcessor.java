c0eda5963f68591024201855b8fa2a8dc7906701
Afonso Matumona <afonso.matumona@minfin.gov.ao>
2025-03-25 14:14:37 +0100

    fix: moved the shared methods to the MappingActiions class

--- src/main/java/io/github/afonsomatelias/Mapper/Interfaces/IListProcessor.java
+++ src/main/java/io/github/afonsomatelias/Mapper/Interfaces/IListProcessor.java
@@ -3,12 +3,11 @@ package io.github.afonsomatelias.Mapper.Interfaces;
 import java.util.List;
 
 import io.github.afonsomatelias.Callback.ICallbacks.CallbackV1;
-import io.github.afonsomatelias.Options.Interfaces.IMappingActions;
 import io.github.afonsomatelias.Options.Interfaces.IMappingListActions;
 
 public interface IListProcessor<S> extends IProcessor<S> {
 	/**
-	 * Converts the list of {@link S} objects to the list of destination class
+	 * Maps the list of {@link S} objects to the list of destination class
 	 * provided
 	 * 
 	 * @param <D>   the {@link D} object type
@@ -16,18 +15,9 @@ public interface IListProcessor<S> extends IProcessor<S> {
 	 * @return the object Converted
 	 */
 	public <D> List<D> to(Class<D> clazz);
-
+	
 	/**
-	 * Creates a new instance of object provided, just like copy and paste with
-	 * different memory address
-	 * 
-	 * @param modifier mapping options that will be applied on map
-	 * @return new object instance
-	 */
-	public <D extends S> S to(CallbackV1<IMappingActions<S, D>> modifier);
-
-	/**
-	 * Converts the list of {@link S} objects to the list of destination class
+	 * Maps the list of {@link S} objects to the list of destination class
 	 * provided with a mapper modifier
 	 * 
 	 * @param <D>      the {@link D} object type
@@ -36,4 +26,14 @@ public interface IListProcessor<S> extends IProcessor<S> {
 	 * @return the object Converted
 	 */
 	public <D> List<D> to(Class<D> clazz, CallbackV1<IMappingListActions<S, D>> modifier);
+
+	/**
+	 * Creates a new instance of the object provided, just like copy and paste with
+	 * a different memory address, applying mapping options if a modifier is provided.
+	 *
+	 * @param <D>      the {@link D} object type which extends {@link S}
+	 * @param modifier mapping options that will be applied on map
+	 * @return new object instance or null in case of an exception
+	 */
+	public <D extends S> S to(CallbackV1<IMappingListActions<S, D>> modifier);
 }
--- src/main/java/io/github/afonsomatelias/Mapper/Interfaces/IObjectProcessor.java
+++ src/main/java/io/github/afonsomatelias/Mapper/Interfaces/IObjectProcessor.java
@@ -1,11 +1,11 @@
 package io.github.afonsomatelias.Mapper.Interfaces;
 
 import io.github.afonsomatelias.Callback.ICallbacks.CallbackV1;
-import io.github.afonsomatelias.Options.Interfaces.IMappingActions;
+import io.github.afonsomatelias.Options.Interfaces.IMappingObjectActions;
 
 public interface IObjectProcessor<S> extends IProcessor<S> {
 	/**
-	 * Converts the {@link S} object to the destination class provided
+	 * Maps the source object to the destination class provided
 	 * 
 	 * @param <D>   the {@link D} object type
 	 * @param clazz the {@link D} class type
@@ -14,40 +14,48 @@ public interface IObjectProcessor<S> extends IProcessor<S> {
 	public <D> D to(Class<D> clazz);
 
 	/**
-	 * Converts the {@link S} object to the destination class provided with a
-	 * mapper modifier
+	 * Maps the {@link S} object to the destination class provided with a mapper modifier
 	 * 
 	 * @param <D>      the {@link D} object type
 	 * @param clazz    the {@link D} class type
 	 * @param modifier mapping options that will be applied on map
 	 * @return the object Converted
 	 */
-	public <D> D to(Class<D> clazz, CallbackV1<IMappingActions<S, D>> modifier);
+	public <D> D to(Class<D> clazz, CallbackV1<IMappingObjectActions<S, D>> modifier);
 	
 	/**
-	 * Creates a new instance of object provided, just like copy and paste with
-	 * different memory address
-	 * 
-	 * @param modifier mapping options that will be applied on map
-	 * @return new object instance
-	 */
-	public <D extends S> S to(CallbackV1<IMappingActions<S, D>> modifier);
+     * Creates a new instance of the source object with a different memory address,
+     * applying mapping options if a modifier is provided.
+     *
+     * @param <D>      the {@link D} object type which extends {@link S}
+     * @param modifier mapping options that will be applied on map
+     * @return new object instance or null in case of an exception
+     */
+	public <D extends S> S to(CallbackV1<IMappingObjectActions<S, D>> modifier);
 
 	/**
-	 * Maps or Extracts values from a Converts from the destination to the source
+	 * Maps or extracts values from the provided destination object back to the source object.
 	 * 
-	 * @param <D>   the {@link D} object type
-	 * @return the object Converted
+	 * This method swaps the roles of the source and destination, effectively reversing the mapping process.
+	 * 
+	 * @param <D> the type of the destination object
+	 * @param destination the destination object from which values are mapped to the source
+	 * @return the source object with values mapped from the destination, or null if the destination is null
 	 */
 	public <D> S from(D destination);
 
 	/**
-	 * Maps or Extracts values from a Converts from the destination to the source with a
-	 * mapper modifier
+	 * Maps or extracts values from the provided destination object back to the source object,
+	 * with the option to apply a mapping modifier.
 	 * 
-	 * @param <D>      the {@link D} object type
-	 * @param modifier mapping options that will be applied on map
-	 * @return the object Converted
+	 * This method swaps the roles of the source and destination, effectively reversing the mapping process.
+	 * It triggers BEFORE_MAP and AFTER_MAP actions if modifiers are set. The modifier allows additional
+	 * actions or transformations to be applied during the mapping process.
+	 * 
+	 * @param <D> the type of the destination object
+	 * @param destination the destination object from which values are mapped to the source
+	 * @param modifier a callback allowing custom mapping actions to be applied
+	 * @return the source object with values mapped from the destination, or null if the destination is null
 	 */
-	public <D> S from(D destination, CallbackV1<IMappingActions<D, S>> modifier);
+	public <D> S from(D destination, CallbackV1<IMappingObjectActions<D, S>> modifier);
 }
