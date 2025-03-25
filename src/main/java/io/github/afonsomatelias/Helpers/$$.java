7d7750fd8b2196b53475cb29f50be80494f374a8
Afonso Matumona <afonso.matumona@minfin.gov.ao>
2025-03-25 11:43:42 +0100

    feat: added one more logger class to help in global log config apply

---
+++ src/main/java/io/github/afonsomatelias/Helpers/$$.java
@@ -0,0 +1,15 @@
+package io.github.afonsomatelias.Helpers;
+
+// Hiden printer to controller the what to print in the converter 
+public class $$ {
+    public static boolean isSilent = false;
+
+    public static void out(Object... contents) {
+        if (isSilent) return;
+        Printer.out(contents);
+    }
+
+    public static void err(Object... contents) {
+        Printer.err(contents);
+    }
+}
