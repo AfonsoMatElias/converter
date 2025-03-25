2d1977517fc1042f2f47097b65638a2bf678d072
Afonso Matumona <afonso.matumona@minfin.gov.ao>
2025-03-25 11:41:26 +0100

    feat: added silent logs to avoid printing warnings to the console

--- src/main/java/io/github/afonsomatelias/Configurations/ConverterShared.java
+++ src/main/java/io/github/afonsomatelias/Configurations/ConverterShared.java
@@ -8,18 +8,20 @@ import java.util.Map;
 import java.util.Set;
 
 import io.github.afonsomatelias.Callback.ICallbacks.CallbackP1;
-import io.github.afonsomatelias.Options.MappingActions;
+import io.github.afonsomatelias.Options.MappingObjectActions;
 import io.github.afonsomatelias.Options.MemberMapping.FieldMemberMapping;
 import io.github.afonsomatelias.Options.MemberMapping.SetterMemberMapping;
 
 public class ConverterShared {
+	public boolean SILENT_LOGS = false;
 	public boolean USE_MAPPING_CONFIG = false;
-	public final Map<String, MapperConfig> configurations = new HashMap<>();
+	
+	public final Map<String, MappingConfig> configurations = new HashMap<>();
 	public final Map<String, CallbackP1<Object, Object>> tranformations = new HashMap<>();
 
 	public final Map<Field, FieldMemberMapping> forMemberMapping = new HashMap<>();
 	public final Map<String, List<SetterMemberMapping>> forSetterMemberMapping = new HashMap<>();
-	public final Map<String, MappingActions<Object, Object>> globalActionOptions = new HashMap<>();
+	public final Map<String, MappingObjectActions<Object, Object>> globalActionOptions = new HashMap<>();
 
 	public final Set<String> classTypesToIgnore = new HashSet<>();
 }
